36d295dd5320b1e39ac0d47209fe80f3529e4eec
pawelkulpa <pablo.kulpa@gmail.com>
2018-02-21 12:42:18 +0100

    Implement threads.

--- src/main/java/hello/DataRetriever.java
+++ src/main/java/hello/DataRetriever.java
@@ -1,5 +1,8 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -7,14 +10,15 @@ import java.util.Arrays;
 
 public class DataRetriever extends Thread{
 
-    RestTemplate restTemplate;
+    RestTemplate restTemplate = new RestTemplate();
+
 
     public void run() {
 
 
         while (true){
             try {
-                Thread.sleep(10000);
+                Thread.sleep(7000);
                 ResponseEntity<CryptoCurrency[]> cryptoCurrencies = restTemplate
                         .getForEntity("https://api.coinmarketcap.com/v1/ticker",CryptoCurrency[].class);
 
