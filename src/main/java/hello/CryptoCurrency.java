58986f15187d066abb9dd525d434c649ec74f752
Ernest <ernestjaroszek22@gmail.com>
2018-02-21 10:27:34 +0100

    cryptocurrency model

---
+++ src/main/java/hello/CryptoCurrency.java
@@ -0,0 +1,130 @@
+package hello;
+
+public class CryptoCurrency {
+    private String name;
+    private String symbol;
+    private int rank;
+    private double priceUSD;
+    private double priceBTC;
+    private double usdVolume24h;
+    private double marketCapUSD;
+    private double availableSupply;
+    private double totalSupply;
+    private double maxSupply;
+    private double percentChange1h;
+    private double percentChange24h;
+    private double percentChange7d;
+    private double lastUpdated;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public double getPriceUSD() {
+        return priceUSD;
+    }
+
+    public void setPriceUSD(double priceUSD) {
+        this.priceUSD = priceUSD;
+    }
+
+    public double getPriceBTC() {
+        return priceBTC;
+    }
+
+    public void setPriceBTC(double priceBTC) {
+        this.priceBTC = priceBTC;
+    }
+
+    public double getUsdVolume24h() {
+        return usdVolume24h;
+    }
+
+    public void setUsdVolume24h(double usdVolume24h) {
+        this.usdVolume24h = usdVolume24h;
+    }
+
+    public double getMarketCapUSD() {
+        return marketCapUSD;
+    }
+
+    public void setMarketCapUSD(double marketCapUSD) {
+        this.marketCapUSD = marketCapUSD;
+    }
+
+    public double getAvailableSupply() {
+        return availableSupply;
+    }
+
+    public void setAvailableSupply(double availableSupply) {
+        this.availableSupply = availableSupply;
+    }
+
+    public double getTotalSupply() {
+        return totalSupply;
+    }
+
+    public void setTotalSupply(double totalSupply) {
+        this.totalSupply = totalSupply;
+    }
+
+    public double getMaxSupply() {
+        return maxSupply;
+    }
+
+    public void setMaxSupply(double maxSupply) {
+        this.maxSupply = maxSupply;
+    }
+
+    public double getPercentChange1h() {
+        return percentChange1h;
+    }
+
+    public void setPercentChange1h(double percentChange1h) {
+        this.percentChange1h = percentChange1h;
+    }
+
+    public double getPercentChange24h() {
+        return percentChange24h;
+    }
+
+    public void setPercentChange24h(double percentChange24h) {
+        this.percentChange24h = percentChange24h;
+    }
+
+    public double getPercentChange7d() {
+        return percentChange7d;
+    }
+
+    public void setPercentChange7d(double percentChange7d) {
+        this.percentChange7d = percentChange7d;
+    }
+
+    public double getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(double lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
